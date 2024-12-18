a07a389cce352316b634e2343ab786303284bf55
diogo-sant0s <githubpersonal0@gmail.com>
2024-12-18 12:10:14 +0000

    changed the language of the python file from portuguese to english

--- contas.py
+++ contas.py
@@ -1,21 +1,21 @@
 a = 0
 b = 0
-subtração = 0
-soma = 0
-divisão = 0
-multiplicação = 0
+subtraction = 0
+sum = 0
+division = 0
+multiplication = 0
 
-a = int(input("Introduza um número inteiro"))
-b = int(input("Introduza outro número inteiro"))
+a = int(input("Introduza um número inteiro:"))
+b = int(input("Introduza outro número inteiro:"))
 
-subtração = a - b
-soma = a + b
-divisão = a / b
-multiplicação = a * b 
+subtraction = a - b
+sum = a + b
+division = a / b
+multiplication = a * b 
 
 
-print("Resultado da subtração:", subtração)
-print("Resultado da soma", soma)
-print("Resultado da divisão", divisão)
-print("Resultado da multriplicação", multiplicação)
+print("Resultado da subtração:", subtraction)
+print("Resultado da soma", sum)
+print("Resultado da divisão", division)
+print("Resultado da multriplicação", multiplication)
       
