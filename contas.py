c561e27479eb2735501ad75edb7331f0c4df62ef
diogo-sant0s <githubpersonal0@gmail.com>
2024-12-18 12:26:09 +0000

    simplified the python code

--- contas.py
+++ contas.py
@@ -1,21 +1,21 @@
 a = 0
 b = 0
-subtraction = 0
 sum = 0
+subtraction = 0
 division = 0
 multiplication = 0
 
 a = int(input("Introduza um número inteiro:"))
 b = int(input("Introduza outro número inteiro:"))
 
-subtraction = a - b
 sum = a + b
+subtraction = a - b
 division = a / b
 multiplication = a * b 
 
 
-print("Resultado da subtração:", subtraction)
-print("Resultado da soma", sum)
-print("Resultado da divisão", division)
-print("Resultado da multriplicação", multiplication)
+print(f"Resultado da subtração {a} - {b} = {sum}")
+print(f"Resultado da soma {a} + {b} = {subtraction}")
+print(f"Resultado da divisão {a} / {b} = {division}")
+print(f"Resultado da multiplicação {a} * {b} = {multiplication}")
       
